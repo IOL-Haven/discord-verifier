0f06ee56c32838ddbed20ab9999ee9980bd08141
Jonah Janzen <jonah.janzen@gmail.com>
2024-08-21 07:27:50 -0600

    Initial Commit

---
+++ src/client/java/org/iolhaven/discordverifier/DiscordVerifierClient.java
@@ -0,0 +1,10 @@
+package org.iolhaven.discordverifier;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DiscordVerifierClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
