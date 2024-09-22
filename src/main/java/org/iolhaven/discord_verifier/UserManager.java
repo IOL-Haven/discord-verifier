a0126eb10cc6a25ac5ca3250b4407e23a4927ab8
Jonah Janzen <jonah.janzen@gmail.com>
2024-09-22 11:07:52 -0400

    Use a custom type for storing user data
    The Minecraft `Pair<A,B>` type has issues with serialization and deserialization due to it being a generic type.
    To ensure that data is always stored and loaded properly, the user manager now uses its own datatype for storing the associations between Discord IDs and Minecraft usernames.

--- src/main/java/org/iolhaven/discord_verifier/UserManager.java
+++ src/main/java/org/iolhaven/discord_verifier/UserManager.java
@@ -6,7 +6,6 @@ import com.google.gson.reflect.TypeToken;
 import com.mojang.authlib.GameProfile;
 import discord4j.common.util.Snowflake;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.util.Pair;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -22,6 +21,16 @@ public final class UserManager {
     // Handle to the list of stored users
     private static final File USER_RECORD = new File(FabricLoader.getInstance().getConfigDir().toFile(), "discord_verifier/users.json");
 
+    private class UserPair {
+        public Snowflake discord;
+        public String minecraft;
+
+        public UserPair(Snowflake _discord, String _minecraft) {
+            discord = _discord;
+            minecraft = _minecraft;
+        }
+    }
+
     private UserManager() {
         // Create a new blank user record if it does not exist already.
         if(!USER_RECORD.exists()) {
@@ -31,7 +40,7 @@ public final class UserManager {
                 }
                 USER_RECORD.createNewFile();
                 try (Writer writer = new FileWriter(USER_RECORD)) {
-                    GSON.toJson(new ArrayList<Pair<Snowflake, String>>(), writer);
+                    GSON.toJson(new ArrayList<UserPair>(), writer);
                 }
 
                 DiscordVerifier.LOGGER.debug("Wrote a new blank user record.");
@@ -53,14 +62,15 @@ public final class UserManager {
     // If the Discord account already had a Minecraft username associated with it, the function will return the name of the previous Minecraft account, which gets overwritten.
     // Otherwise, it returns nothing.
     public Optional<String> writeUsername(Snowflake discord, String minecraft) {
-        List<Pair<Snowflake, String>> users;
+        List<UserPair> users;
         Optional<String> out = Optional.empty();
+
         try (Reader reader = new FileReader(USER_RECORD)) {
             // Load the user record
             users = GSON.fromJson(reader, new TypeToken<>() {});
             out = eraseExistingEntry(users, discord);
             // Add the new user record
-            users.add(new Pair<>(discord, minecraft));
+            users.add(new UserPair(discord, minecraft));
         } catch (Exception e) {
             DiscordVerifier.LOGGER.error("Error loading users: {}", e.getMessage());
             return out;
@@ -80,21 +90,21 @@ public final class UserManager {
     }
 
     // Removes and returns any existing entries associated with the given Discord ID
-    private static Optional<String> eraseExistingEntry(List<Pair<Snowflake, String>> users, Snowflake discord) {
+    private static Optional<String> eraseExistingEntry(List<UserPair> users, Snowflake discord) {
         // There won't be multiple entries matching the condition in the list, so the join() functionality will never get used.
-        String out = String.join(", ", users.stream().filter(pair -> pair.getLeft().equals(discord)).map(Pair::getRight).toList());
-        users.removeIf(pair -> pair.getLeft().equals(discord));
+        String out = String.join(", ", users.stream().filter(pair -> pair.discord.equals(discord)).map(pair -> pair.minecraft).toList());
+        users.removeIf(pair -> pair.discord.equals(discord));
         // If there was a username in the output string, return it. Otherwise, convert an empty string to an empty Optional.
         return out.isEmpty() ? Optional.empty() : Optional.of(out);
     }
 
     // Checks to see whether a Minecraft profile has been verified through Discord, and whether it is thus allowed to join.
     public boolean userIsVerified(GameProfile minecraftUser) {
-        List<Pair<Snowflake, String>> users;
+        List<UserPair> users;
         try (Reader reader = new FileReader(USER_RECORD)) {
             users = GSON.fromJson(reader, new TypeToken<>() {});
             // If there's any match in the record of verified users, they're verified!
-            return users.stream().anyMatch(pair -> matchMinecraftUsername(minecraftUser, pair.getRight()));
+            return users.stream().anyMatch(pair -> matchMinecraftUsername(minecraftUser, pair.minecraft));
         } catch (Exception e) {
             DiscordVerifier.LOGGER.error("Error checking user verification: {}", e.getMessage());
             return false;
